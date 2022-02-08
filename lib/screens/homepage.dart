0a42fd406b2897603def5ee8cceca3a3e86365a3
Red Skull <redskullcoder@gmail.com>
2022-02-07 21:48:21 -0800

    VIFI08 - BOTTOM NAVIGATION BAR ITEM

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:vifi_meet/variables.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,9 +8,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int page = 0;
+  List pageoptions = [
+    videoConferenceScreen(),
+    ProfileScreen(),
+  ]
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //NAV BOTTOM
+      backgroundColor: Colors.grey,
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.blue,
+        selectedLabelStyle: mystyle(17, Colors.blue),
+        unselectedItemColor: Colors.black,
+        unselectedLabelStyle: mystyle(17, Colors.black),
+        currentIndex: page,
+        onTap: (index) {
+          setState(() {
+            page = index;
+          });
+        }, //ELEMENTS NAVIGATION
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.video_call, size: 32), label: 'Video llamada'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person, size: 32), label: 'perfil'),
+        ],
+      ),
       body: InkWell(
         onTap: () {
           FirebaseAuth.instance.signOut();
