23a919652a460dd68a307db5f5126f2b90f0e46d
Red Skull <redskullcoder@gmail.com>
2022-02-08 18:59:36 -0800

    VIFI11 - CREATE TAB BAR

--- lib/screens/videoconferencescreen.dart
+++ lib/screens/videoconferencescreen.dart
@@ -1,13 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gradient_colors/flutter_gradient_colors.dart';
+import 'package:vifi_meet/variables.dart';
+import 'package:vifi_meet/videoconference/createmeeting.dart';
+import 'package:vifi_meet/videoconference/joinmeeting.dart';
 
 class VideoConferenceScreen extends StatefulWidget {
   @override
   _VideoConferenceScreenState createState() => _VideoConferenceScreenState();
 }
 
-class _VideoConferenceScreenState extends State<VideoConferenceScreen> {
+class _VideoConferenceScreenState extends State<VideoConferenceScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
+  buildtab(String name) {
+    return Container(
+      width: 170,
+      height: 50,
+      child: Card(
+        child: Center(
+          child: Text(
+            name,
+            style: mystyle(12, Colors.black, FontWeight.w700),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //MANGE THE TABS LOGIC
+    tabController = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.indigo[600],
+        title: Text(
+          "VIFI",
+          style: mystyle(17, Colors.white, FontWeight.w700),
+        ),
+        bottom: TabBar(
+          controller: tabController,
+          tabs: [
+            buildtab("Ingresar a una Reunión"),
+            buildtab("Crear una Reunión"),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: tabController,
+        children: [
+          JoinMeeting(),
+          CreateMeeting(),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/videoconference/createmeeting.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CreateMeeting extends StatefulWidget {
+  @override
+  _CreateMeetingState createState() => _CreateMeetingState();
+}
+
+class _CreateMeetingState extends State<CreateMeeting> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
