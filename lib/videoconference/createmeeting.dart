ed94dc30bfc930a4d175256ece38736a164888cd
Red Skull <redskullcoder@gmail.com>
2022-02-08 19:26:54 -0800

    VIFI12 - DONE SCREEN CODE

--- lib/videoconference/createmeeting.dart
+++ lib/videoconference/createmeeting.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gradient_colors/flutter_gradient_colors.dart';
+import 'package:uuid/uuid.dart';
+import 'package:vifi_meet/variables.dart';
 
 class CreateMeeting extends StatefulWidget {
   @override
@@ -6,8 +9,62 @@ class CreateMeeting extends StatefulWidget {
 }
 
 class _CreateMeetingState extends State<CreateMeeting> {
+  String code = '';
+  createcode() {
+    setState(() {
+      code = Uuid().v1().substring(0, 6);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            child: Text("Crea un código de reunión y compártelo ",
+                style: mystyle(20), textAlign: TextAlign.center),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                "Código",
+                style: mystyle(30),
+              ),
+              Text(
+                code,
+                style: mystyle(30, Colors.deepPurple, FontWeight.w700),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          InkWell(
+            onTap: () => createcode(),
+            child: Container(
+              width: MediaQuery.of(context).size.width / 2,
+              height: 50,
+              decoration: BoxDecoration(
+                gradient:
+                    LinearGradient(colors: GradientColors.facebookMessenger),
+              ),
+              child: Center(
+                child: Text(
+                  "Crea un código",
+                  style: mystyle(20, Colors.white),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
